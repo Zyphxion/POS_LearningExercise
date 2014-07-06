4eb889d2902a3ef8529ab9ac96f0e5ca394378a7
Zyphxion <zyph25@gmail.com>
2014-07-06 01:55:18 -0700

    Removal
    Removal of uneeded files

--- PointOfSaleSystem/Controllers/ReportsController.cs
+++ PointOfSaleSystem/Controllers/ReportsController.cs
@@ -19,9 +19,6 @@ namespace PointOfSaleSystem.Controllers
         public ActionResult Index()
         {
             List<Sale> sales = db.Sales.ToList();
-            //Item_Sale obj = new Item_Sale();
-            //obj.sales = db.Sales.ToList();
-            //obj.items = db.Items.ToList();
             return View(sales);
         }
 
@@ -52,63 +49,7 @@ namespace PointOfSaleSystem.Controllers
             return View(sale);
         }
 
-        // GET: /Reports/Create
-        public ActionResult Create()
-        {
-            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Description");
-            return View();
-        }
-
-        // POST: /Reports/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="SaleId,TransactionId,ItemId,AmountSold")] Sale sale)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Sales.Add(sale);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Description", sale.ItemId);
-            return View(sale);
-        }
-
-        // GET: /Reports/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Sale sale = db.Sales.Find(id);
-            if (sale == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Description", sale.ItemId);
-            return View(sale);
-        }
-
-        // POST: /Reports/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="SaleId,TransactionId,ItemId,AmountSold")] Sale sale)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(sale).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Description", sale.ItemId);
-            return View(sale);
-        }
+      
 
         // GET: /Reports/Delete/5
         public ActionResult Delete(int? id)
